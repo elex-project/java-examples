1a7710b1007a4cb45d6cfd97743f81c2c7d155ba
Elex <developer@elex-project.com>
2021-08-17 21:49:24 +0900

    2021-08-17

--- thymeleaf/src/test/java/kr/pe/elex/examples/ThymeleafTest.java
+++ thymeleaf/src/test/java/kr/pe/elex/examples/ThymeleafTest.java
@@ -20,8 +20,9 @@ class ThymeleafTest {
 	@Test
 	void test() throws IOException {
 		Context context = new Context();
-		///context.setLocale(Locale.ENGLISH);
+		//context.setLocale(Locale.ENGLISH);
 		context.setVariable("person", new Person("Charlie", 14));
+		context.setVariable("html", "<strong>Hahaha</strong>");
 
 		Thymeleaf thymeleaf = new Thymeleaf();
 		String out = thymeleaf.process("sample.html", context);
